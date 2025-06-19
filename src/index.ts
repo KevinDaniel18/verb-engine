d90ebedb91e651ad2ba2bca586c43a99c21e5827
KevinDaniel18 <kevnsc18@gmail.com>
2025-06-18 20:41:25 -0500

    package.json modified

---
+++ src/index.ts
@@ -0,0 +1,4 @@
+export { conjugateEnglish } from './conjugation/languages/english/english-conjugator';
+export { conjugateSpanish } from './conjugation/languages/spanish/spanish-conjugator';
+
+export type { ConjugationResult } from './conjugation/types/conjugation.types';
--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,11 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { SPANISH_IRREGULARS } from './conjugation/languages/spanish/irregulars';
+import { ENGLISH_IRREGULARS } from './conjugation/languages/english/irregulars';
 
 async function bootstrap() {
   console.log('Verbos irregulares:', Object.keys(SPANISH_IRREGULARS));
+  console.log('Irregular verbs:', Object.keys(ENGLISH_IRREGULARS));
   const app = await NestFactory.create(AppModule);
   await app.listen(process.env.PORT ?? 3000);
 }

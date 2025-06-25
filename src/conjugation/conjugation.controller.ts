f90c689eac1fafd538bbaa378235b50ba335f4e2
KevinDaniel18 <kevnsc18@gmail.com>
2025-06-25 15:15:17 -0500

    new endpoints

--- src/conjugation/conjugation.controller.ts
+++ src/conjugation/conjugation.controller.ts
@@ -1,6 +1,8 @@
 import { BadRequestException, Controller, Get, Query } from '@nestjs/common';
 import { ConjugationService } from './conjugation.service';
 import { ConjugationResult } from './types/conjugation.types';
+import { VALID_ENGLISH_VERBS } from './languages/english/verbs-en';
+import { VALID_SPANISH_VERBS } from './languages/spanish/verbs-es';
 
 @Controller('conjugate')
 export class ConjugationController {
@@ -20,6 +22,24 @@ export class ConjugationController {
         verb.toLocaleLowerCase(),
         lang.toLocaleLowerCase(),
       );
+    } catch (error) {
+      throw new BadRequestException(error.message);
+    }
+  }
+
+  @Get('valid-english-verbs')
+  getValidEnglishVerbs() {
+    try {
+      return { verbs: Array.from(VALID_ENGLISH_VERBS) };
+    } catch (error) {
+      throw new BadRequestException(error.message);
+    }
+  }
+
+  @Get('valid-spanish-verbs')
+  getValidSpanishVerbs() {
+    try {
+      return { verbs: Array.from(VALID_SPANISH_VERBS) };
     } catch (error) {
       throw new BadRequestException(error.message);
     }

075b13e39f0849cf3aa0bcf1b8e0fc6bede05ca3
KevinDaniel18 <kevnsc18@gmail.com>
2025-06-25 17:53:36 -0500

    dockerfile

--- src/main.ts
+++ src/main.ts
@@ -20,11 +20,19 @@ export {
   conjugateSpanish,
   getSpanishVerbs,
 } from './conjugation/languages/spanish/spanish-conjugator';
-
+import { CorsOptions } from '@nestjs/common/interfaces/external/cors-options.interface';
 export type { ConjugationResult } from './conjugation/types/conjugation.types';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  const corsOptions: CorsOptions = {
+    origin: '*',
+    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
+    allowedHeaders: 'Content-Type, Authorization',
+    preflightContinue: false,
+    optionsSuccessStatus: 204,
+  };
+  app.enableCors(corsOptions);
   await app.listen(process.env.PORT ?? 3000);
 }
 bootstrap();
